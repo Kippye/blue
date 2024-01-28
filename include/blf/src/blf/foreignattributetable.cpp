54e0898a9dcc56083281c4fb993dd018ba9486c3
Functionable <40835042+Functionable@users.noreply.github.com>
2024-01-28 12:21:46 +0000

    Remove old BLF library

--- include/blf/src/blf/foreignattributetable.cpp
+++
@@ -1,9 +0,0 @@
-#include "blf/foreignattributetable.hpp"
-
-namespace blf
-{
-    void ForeignAttributeTable::addDefinition(ObjectAttribute definition)
-    {
-        attributes.push_back(definition);
-    }
-}
